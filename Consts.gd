a5f915a6b64f25298ecc789625e81d2adfef71f9
pitrak1 <pitrak1@gmail.com>
2022-01-22 18:30:32 -0600

    create first test level

--- Consts.gd
+++ Consts.gd
@@ -6,3 +6,15 @@ const TILE_WIDTH = 55
 const TILE_HEIGHT = 40
 
 const START_POSITION = Vector2(330, 110)
+
+const LEVEL_1 = [
+	[null, 	null, 	HIGH, 	MIDDLE, 	LOW, 	LOW, 	null, 	null, 	null],
+	[null, 	ROCK, 	HIGH, 	LOW, 	LOW, 	LOW, 	LOW, 	null, 	null],
+	[HIGH, 	HIGH, 	ROCK, 	LOW, 	WATER, 	TREE, 	LOW, 	LOW, 	null],
+	[MIDDLE,	LOW, 	LOW, 	LOW, 	LOW, 	WATER, 	TREE, 	LOW, 	LOW],
+	[LOW, 	LOW, 	WATER, 	LOW, 	TREE, 	LOW, 	WATER, 	LOW, 	LOW],
+	[LOW, 	LOW, 	TREE, 	WATER, 	LOW, 	LOW, 	LOW, 	LOW, 	MIDDLE],
+	[null, 	LOW, 	LOW, 	TREE, 	WATER, 	LOW, 	ROCK, 	HIGH, 	HIGH],
+	[null, 	null, 	LOW, 	LOW, 	LOW, 	LOW, 	HIGH, 	ROCK, 	null],
+	[null, 	null, 	null, 	LOW, 	LOW, 	MIDDLE, 	HIGH, 	null, 	null]
+]
--- LevelPOC.gd
+++ LevelPOC.gd
@@ -2,17 +2,7 @@ extends Node2D
 
 var levelBuilderScript = preload("res://LevelBuilder.gd")
 
-var __level
-
 func _ready():
-	__level = [
-		[Consts.LOW, Consts.LOW, Consts.LOW, Consts.LOW, null],
-		[Consts.LOW, Consts.ROCK, Consts.LOW, Consts.ROCK, Consts.HIGH],
-		[Consts.LOW, Consts.LOW, Consts.LOW, Consts.MIDDLE, Consts.LOW],
-		[Consts.TREE, Consts.LOW, Consts.LOW, Consts.TREE,Consts. LOW],
-		[null, Consts.WATER, Consts.LOW, Consts.MIDDLE, Consts.LOW]
-	]
-	
 	var __levelBuilder = levelBuilderScript.new()
-	__levelBuilder.run(self, __level)
+	__levelBuilder.run(self, Consts.LEVEL_1)
 	
